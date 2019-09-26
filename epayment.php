d65a642bac5bb8fa3dbd2f93cf4714a4d6d691fe
Christophe Zarebski <kzarebski@digitalgarden.fr>
2019-09-26 15:43:05 +0200

    correct sprintf: now logs errors as expected. + other minor errors

--- epayment.php
+++ epayment.php
@@ -561,6 +561,8 @@ class Epayment extends PaymentModule
             return;
         }
 
+        $w = new PayboxHtmlWriter();
+
         // Load order
         $order = new Order($orderId);
         if (!Validate::isLoadedObject($order)) {
@@ -850,7 +852,7 @@ class Epayment extends PaymentModule
             $paymentName = $this->getHelper()->getDisplayName($this->displayName, $params['cardType']);
             $result = parent::validateOrder($cart->id, $state, $amount, $paymentName, $message, array('transaction_id' => $params['transaction']), null, false, $cart->secure_key);
         } catch (Exception $e) {
-            $this->logFatal(sprintf('Cart %d: Error validating PrestaShop order:', $cart->id, $e->getMessage()));
+            $this->logFatal(sprintf('Cart %d: Error validating PrestaShop order: %s', $cart->id, $e->getMessage()));
         }
 
         if (!$result) {
@@ -924,7 +926,7 @@ class Epayment extends PaymentModule
                 $paymentName = $this->getHelper()->getDisplayName($this->displayName, $params['cardType']);
                 $result = parent::validateOrder($cart->id, $state, $orderAmount, $paymentName, $message, array('transaction_id' => $params['transaction']), null, false, $cart->secure_key);
             } catch (Exception $e) {
-                $this->logFatal(sprintf('Cart %d: Error validating PrestaShop order:', $cart->id, $e->getMessage()));
+                $this->logFatal(sprintf('Cart %d: Error validating PrestaShop order: %s', $cart->id, $e->getMessage()));
             }
 
             if (!$result) {
@@ -1074,7 +1076,7 @@ class Epayment extends PaymentModule
                 $paymentName = $this->getHelper()->getDisplayName($this->displayName, $params['cardType'], '3');
                 $result = parent::validateOrder($cart->id, $state, $orderAmount, $paymentName, $message, $params, null, false, $cart->secure_key);
             } catch (Exception $e) {
-                $this->logFatal(sprintf('Cart %d: Error validating PrestaShop order:', $cart->id, $e->getMessage()));
+                $this->logFatal(sprintf('Cart %d: Error validating PrestaShop order: %s', $cart->id, $e->getMessage()));
             }
 
             if (!$result) {
@@ -1155,7 +1157,7 @@ class Epayment extends PaymentModule
                     $this->getHelper()->updateOrderRecurringDetails($order, $details['amount_paid'] + $platformAmount, 0);
 
                     // Info
-                    $message .= sprintf($this->l('Third payment capture of %s %s done.'), sprintf($amountFormat, $amount), $currency->sign) . "\r\n";
+                    $message  = sprintf($this->l('Third payment capture of %s %s done.'), sprintf($amountFormat, $amount), $currency->sign) . "\r\n";
                     $message .= $this->l('No more capture is pending.') . "\r\n";
                     $this->getHelper()->addOrderNote($order, $message);
                     $this->logDebug(sprintf('Cart %d: %s', $cart->id, $message));
@@ -1217,7 +1219,9 @@ class Epayment extends PaymentModule
         $file = sprintf('%s/logs/log_%s.log', dirname(__FILE__), date('Y-m-d'));
         $dir = dirname($file);
         if (!is_dir($dir)) {
-            @mkdir($dir, 0777, true);
+            if (!mkdir($dir, 0777, true) && !is_dir($dir)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
+            }
         }
         file_put_contents($file, $message, FILE_APPEND);
     }
