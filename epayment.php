39ae96d3e3110a71b237818d4455a6dc4bafceda
jerome cintas <jcpaybox@users.noreply.github.com>
2021-04-21 14:22:00 +0200

    version update

--- epayment.php
+++ epayment.php
@@ -13,7 +13,7 @@
 * support@paybox.com so we can mail you a copy immediately.
 *
 *  @category  Module / payments_gateways
-*  @version   3.0.20
+*  @version   3.0.201
 *  @author    BM Services <contact@bm-services.com>
 *  @copyright 2012-2017 Verifone e-commerce
 *  @license   http://opensource.org/licenses/OSL-3.0
@@ -46,7 +46,7 @@ class Epayment extends PaymentModule
 
         $this->name = 'epayment';
         $this->tab = 'payments_gateways';
-        $this->version = '3.0.20';
+        $this->version = '3.0.201';
         $this->author = 'Verifone e-commerce';
         $this->bootstrap = true;
 
